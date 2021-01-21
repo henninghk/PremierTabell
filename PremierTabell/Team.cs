dae4bcbef556692cf4467dcef06ce0899cbdbb30
henninghk <64491564+henninghk@users.noreply.github.com>
2021-01-21 09:35:17 +0100

    Initial commit

---
+++ PremierTabell/Team.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PremierTabell
+{
+    class Team
+    {
+        public string _name;
+        public int _gameplayed;
+        public int _gamewon;
+        public int _gamedraw;
+        public int _gamelost;
+        public int _goaldiff;
+        public int _points;
+
+
+
+        public Team(string name, int gameplayed, int gamewon, int gamedraw, int gamelost, int goaldiff, int points)
+        {
+         _name = name;
+         _gameplayed = gameplayed;
+         _gamewon = gamewon;
+         _gamedraw = gamedraw;
+         _gamelost = gamelost;
+         _goaldiff = goaldiff;
+         _points = points;
+    
+        }
+        public void show() 
+        {
+            Console.WriteLine(_name+"\t"+_gameplayed+"\t"+_gamewon+"\t"+_gamedraw + "\t" + _gamelost + "\t" + _goaldiff + "\t" + _points);
+        }
+    }
+}
